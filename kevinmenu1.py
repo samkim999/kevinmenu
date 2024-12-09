4bef8b108a7c857f5c6225e259036283d867ea14
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2024-12-09 20:35:49 +1000

    add 1st time
    add 1st time menu

---
+++ kevinmenu1.py
@@ -0,0 +1,50 @@
+import streamlit as st
+
+st.write("# Kevin 의  중요 사이트 ! 👋")
+
+with st.sidebar:
+     
+     st.markdown(
+      """
+          언어 번역기 
+      
+          - [Google 번역 ](https://translate.google.com/?sl=ko&tl=en&op=translate)
+          - [파파고 번역 ](https://papago.naver.com/)
+          
+          Naver 사전 
+      
+          - [Naver 번역 ](https://en.dict.naver.com)
+
+          Britannica 백과사전 
+      
+          - [영국 백과사전 ](https://namu.wiki/w/%EB%B8%8C%EB%A6%AC%ED%83%9C%EB%8B%88%EC%BB%A4%20%EB%8C%80%EB%B0%B1%EA%B3%BC%EC%82%AC%EC%A0%84)
+
+          Youtube  
+      
+          - [유트부 보기 ](https://www.youtube.com/)
+
+
+
+                 
+    """
+    )
+     
+
+st.markdown(
+    """
+    살다 보면 즐거운 일도 있지만 말못할 고민이 있게 됩니다.
+    사람한태 물어 보가 힘들때   
+    여기 Kevin 의 심리 상담 챗봇 한테 문의를 ..
+
+
+    - [심리 상담 챗봇 ](https://2024-app4-8yc8qc8xa6jacxc3mjtyw4.streamlit.app/)
+    
+    
+    업무를 하다보면 문서를 번역 한다던지 요약한다던지 내용의 의미를 묻고 싶을때 
+    Kevin 의 챗봇을 통해 물어 보세요.  먼저 볼 File 를 upload 하시고   
+    
+    - [Kevin 의 챗봇 ](https://langchain6-gwtklymlqjbtytbcaestpf.streamlit.app/)
+
+     
+"""
+)
